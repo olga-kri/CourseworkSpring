acddb3dc0434046e5c11f305ee4c93410a669bc2
olga-kri <krivenkoolga1987@gmail.com>
2024-01-29 03:00:18 +0500

    Initial commit

---
+++ src/main/java/pro/sky/demo/CourseworkSpringApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseworkSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseworkSpringApplication.class, args);
+	}
+
+}
